638ee96b3fb6e964e3873b1f321ceee88ef8d2bd
Marian Stefanescu <marianstefi20@gmail.com>
2024-04-13 11:45:27 +0300

    Ignore extra settings from .env

--- concurrent_openai/settings.py
+++ concurrent_openai/settings.py
@@ -36,7 +36,9 @@ class Settings(BaseSettings):
         },
     }
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
 
 
 settings = Settings()
