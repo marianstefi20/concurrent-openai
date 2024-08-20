4eb85a536d0140f874e26e13726dced0e9681e7a
Marian Stefanescu <marianstefi20@gmail.com>
2024-08-21 00:34:01 +0300

    Add repository link to project

--- concurrent_openai/settings.py
+++ concurrent_openai/settings.py
@@ -15,25 +15,36 @@ class Settings(BaseSettings):
             "tkm": 300_000,
             "input_token_cost": 10 / 1_000_000,
             "output_token_cost": 30 / 1_000_000,
+            "max_tokens": 8092,
         },
         "gpt-4-turbo-preview": {
             "rpm": 10_000,
             "tkm": 1_500_000,
             "input_token_cost": 10 / 1_000_000,
             "output_token_cost": 30 / 1_000_000,
+            "max_tokens": 8092,
         },
         "gpt-3.5-turbo": {
             "rpm": 10_000,
             "tkm": 2_000_000,
             "input_token_cost": 10 / 1_000_000,
             "output_token_cost": 30 / 1_000_000,
+            "max_tokens": 4096,
         },
         "gpt-4-1106-vision-preview": {
             "rpm": 3000,
             "tkm": 250000,
             "input_token_cost": 0.00001,
             "output_token_cost": 0.00003,
+            "max_tokens": 4096,
         },
+        "gpt-4o": {
+            "rpm": 10_000,
+            "tkm": 20_000_000,
+            "input_token_cost": 5 / 1_000_000,
+            "output_token_cost": 15 / 1_000_000,
+            "max_tokens": 4096,
+        }
     }
 
     model_config = SettingsConfigDict(
